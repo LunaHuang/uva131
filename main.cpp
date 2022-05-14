1143eb6a13996af154a203723cf33c9b305b7161
Luna Huang <lunahuang@weintek.com>
2022-05-14 12:00:12 +0800

    update

--- main.cpp
+++ main.cpp
@@ -5,59 +5,122 @@
 #include <regex>
 #include <cctype>
 #include <map>
-#include <deque>
 #include <vector>
 
 #if defined(WITH_GTEST)
 #include <gtest/gtest.h>
 #endif
 
-class Train
+class Poker
 {
-
+    // constructions
 public:
-    void show(void)
-    {
-        for (auto &it : on_rail_) {
-            std::cout << it << " ";
-        }
-        std::cout << std::endl;
-    }
+    explicit Poker(std::string &input);
 
-    void operator<<(const std::string &line)
-    {
-        char *token = strtok(const_cast< char * >(line.c_str()), " ");
-        while (token != nullptr) {
-            on_rail_.push_back(std::stoi(std::string(token)));
-            token = strtok(nullptr, " ");
-        }
-    }
+    // methods
+public:
+    void show(void);
+    void show_map(void);
+    std::string display_poker(void);
+    std::string get_bast_combination(void);
 
 private:
-    std::deque< int > on_rail_;
-    std::deque< int > on_station_;
+    void poker_to_map(void);
+
+    // data
+private:
+    std::vector< std::string > poker_;
+    std::multimap< int, std::string > poker_map_;
 };
+
+Poker::Poker(std::string &input)
+{
+    std::string token;
+    std::stringstream ss(input);
+    while (std::getline(ss, token, ' '))
+        poker_.push_back(token);
+}
+
+void Poker::show(void)
+{
+    for (auto &it : poker_)
+        std::cout << it << " ";
+    std::cout << std::endl;
+}
+
+void Poker::show_map(void)
+{
+    for (auto &it : poker_map_)
+        std::cout << it.first << " " << it.second << " ";
+    std::cout << std::endl;
+}
+
+std::string Poker::display_poker(void)
+{
+    int i = 0;
+    std::string out;
+    for (auto pos = poker_.cbegin(); pos != poker_.cend(); ++pos) {
+        if (i == 0)
+            out = "Hand: " + *pos + " ";
+        else if (i == 5)
+            out = out + "Deck: " + *pos + " ";
+        else
+            out = out + *pos + " ";
+
+        i++;
+    }
+    return out;
+}
+
+void Poker::poker_to_map(void)
+{
+    for (auto &it : poker_) {
+        std::cout << it << " ";
+        std::string f = it.substr(0, 1);
+        int f_num;
+        if (f == "A")
+            f_num = 1;
+        else if (f == "T")
+            f_num = 10;
+        else if (f == "J")
+            f_num = 11;
+        else if (f == "Q")
+            f_num = 12;
+        else if (f == "K")
+            f_num = 13;
+        else
+            f_num = stoi(f);
+
+        poker_map_.insert({ f_num, it.substr(1, 1) });
+    }
+}
+
+std::string Poker::get_bast_combination()
+{
+    poker_to_map();
+    show_map();
+    std::string combine;
+    combine = "Best hand: ";
+    return combine;
+}
+
+void poker_combination(std::string input, std::ostream &os)
+{
+    Poker poker(input);
+    //    poker.show();
+    std::string out = poker.display_poker();
+    out = out + poker.get_bast_combination();
+    os << out << std::endl;
+}
 
 void solve_uva_problem(std::istream &is, std::ostream &os)
 {
-    std::string numbers;
-    std::getline(is, numbers);
-    int num = std::stoi(numbers);
     while (1) {
-        std::getline(is, numbers);
-        if (numbers.compare("0") != 0) {
-            if (check_permutations(numbers, num) == true)
-                os << "Yes" << std::endl;
-            else
-                os << "No" << std::endl;
-        } else {
-            os << std::endl;
-            std::getline(is, numbers);
-            if (numbers.compare("0") == 0)
-                break;
-            else
-                num = std::stoi(numbers);
-        }
+        std::string poker_input;
+        std::getline(is, poker_input);
+        if (poker_input.empty())
+            break;
+        poker_combination(poker_input, os);
     }
 }
 
